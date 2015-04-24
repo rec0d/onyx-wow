fa5f7a825815110f128c60fc06bd89197bf9083e
Salja <salja8840@gmail.com>
2015-04-24 10:34:20 +0200

    Eluna Sync
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/LuaEngine/GameObjectMethods.h
+++ src/game/LuaEngine/GameObjectMethods.h
@@ -121,6 +121,32 @@ namespace LuaGameObject
     }
 
     /**
+     * Returns the [Player] that can loot the [GameObject]
+     *
+     * @return [Player] player
+     */
+    int GetLootRecipient(Eluna* /*E*/, lua_State* L, GameObject* go)
+    {
+        Eluna::Push(L, go->GetLootRecipient());
+        return 1;
+    }
+
+    /**
+     * Returns the [Group] that can loot the [GameObject]
+     *
+     * @return [Group] group
+     */
+    int GetLootRecipientGroup(Eluna* /*E*/, lua_State* L, GameObject* go)
+    {
+#ifdef TRINITY
+        Eluna::Push(L, go->GetLootRecipientGroup());
+#else
+        Eluna::Push(L, go->GetGroupLootRecipient());
+#endif
+        return 1;
+    }
+
+    /**
      * Sets the state of a [GameObject]
      *
      * <pre>
